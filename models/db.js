e3314da6dae5c2fdb540343d591f8352e4cdb1fa
celsj <celsj@mcmaster.ca>
2023-09-11 19:15:15 -0400

    Changed db connection to use PlanetScale and env variables

--- models/db.js
+++ models/db.js
@@ -1,19 +1,10 @@
-const mysql = require("mysql");
-const dbConfig = require("../config/db.config.js");
+require('dotenv').config();
+const mysql = require('mysql2');
 
-//Connection params for the database
-const connection = mysql.createConnection({
-  host: dbConfig.HOST,
-  user: dbConfig.USER,
-  password: dbConfig.PASSWORD,
-  database: dbConfig.DB,
-  insecureAuth: true
-});
-
-//Open MySQL connection
+const connection = mysql.createConnection(process.env.DATABASE_URL);
 connection.connect(error => {
-  if (error) throw error;
-  console.log("Successfully connected to the database.");
+    if (error) throw error;
+    console.log("Connected to the database");
 });
 
 module.exports = connection;
